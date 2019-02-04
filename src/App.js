6b428ffad7848298d775a498ccbb0939327f5d7a
bluetechnology <takanori.fukuyama@ngl.technology>
2019-02-05 03:19:23 +0900

    mediaCard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component, Fragment } from 'react';
 
 import './App.css';
 import ButtonAppBar from './components/AppBar';
-import CouponList from './components/CouponList';
+// import CouponList from './components/CouponList';
+import MediaCard from './components/MediaCard';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <ButtonAppBar />
-        <CouponList />
+        <MediaCard />
+        {/* <CouponList /> */}
       </Fragment>
     );
   }
